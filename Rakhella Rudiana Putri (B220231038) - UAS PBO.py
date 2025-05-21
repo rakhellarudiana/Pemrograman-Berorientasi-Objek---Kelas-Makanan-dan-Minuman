28b6e083000c617af197d9c326b0480574812ed5
Rakhella Rudiana Putri <142279996+rakhellarudiana@users.noreply.github.com>
2025-05-22 03:54:40 +0700

    Add files via upload

---
+++ Rakhella Rudiana Putri (B220231038) - UAS PBO.py
@@ -0,0 +1,239 @@
+#UJIAN AKHIR SEMESTER GENAP   | MK: PEMROGRAMAN BERORIENTASI OBJEK (PBO) | DOSEN PENGAMPU: HANDOKO., M.Kom
+#NAMA: RAKHELLA RUDIANA PUTRI | NIM: B220231038
+
+#Membuat Project Pemrograman Berorientasi Objek (yang ada class seperti objek, atribut, method) Dan Dapat Dipakai Dalam Sehari-hari)
+#CLASSES
+#cara pada nomor 1
+class Makanan_dan_Minuman:
+    nama1      = 'permen, cupcake, cokelat, dan soda.'
+    kandungan1 = 'rendah nutrisi, tinggi kalori, lemak, serta gula.'
+    kelompok1  = 'tidak dikonsumsi setiap hari.'
+    nama2      = 'mentega dan minyak.'
+    kandungan2 = 'lemak jenuh dan asam lemak.'
+    kelompok2  = 'dikonsumsi dengan kuantitas yang sedikit.'
+    nama3      = 'daging, ayam, ikan, telur, dan kacang.'
+    kandungan3 = 'protein hewani maupun nabati.'
+    kelompok3  = 'dikonsumsi 1x dalam sehari.'
+    nama4      = 'susu dan produk susu seperti yogurt dan keju.'
+    kandungan4 = 'protein, kalsium, dan vitamin B12.'
+    kelompok4  = 'dikonsumsi 3x sehari untuk orang dewasa, dikonsumsi 5x sehari untuk anak-anak ataupun remaja yang berusia 9 sampai 18 tahun.'
+    nama5      = 'bijirin, roti, kentang, pasta, dan nasi.'
+    kandungan5 = 'karbohidrat, serat, protein.'
+    kelompok5  = 'dikonsumsi 3x sampai 5x sehari.'
+    nama6      = 'salad, sayur dan buah-buahan.'
+    kandungan6 = 'nutrisi seperti vitamin, mineral, serat, dan antioksidan.'
+    kelompok6  = 'dikonsumsi 3x sampai 7x sehari.'
+buah_buahan = Makanan_dan_Minuman()
+print()
+print('-- class Makanan dan Minuman --')
+print('memiliki 3 objek Makanan dan Minuman: ')
+print('nama makanan dan minuman 1     : ',Makanan_dan_Minuman.nama1)
+print('kandungan makanan dan minuman 1: ',Makanan_dan_Minuman.kandungan1)
+print('kelompok makanan dan minuman 1 : ',Makanan_dan_Minuman.kelompok1)
+print('nama makanan dan minuman 2     : ',Makanan_dan_Minuman.nama2)
+print('kandungan makanan dan minuman 2: ',Makanan_dan_Minuman.kandungan2)
+print('kelompok makanan dan minuman 2 : ',Makanan_dan_Minuman.kelompok2)
+print('nama makanan dan minuman 3     : ',Makanan_dan_Minuman.nama3)
+print('kandungan makanan dan minuman 3: ',Makanan_dan_Minuman.kandungan3)
+print('kelompok makanan dan minuman 3 : ',Makanan_dan_Minuman.kelompok3)
+print('nama makanan dan minuman 4     : ',Makanan_dan_Minuman.nama4)
+print('kandungan makanan dan minuman 4: ',Makanan_dan_Minuman.kandungan4)
+print('kelompok makanan dan minuman 4 : ',Makanan_dan_Minuman.kelompok4)
+print('nama makanan dan minuman 5     : ',Makanan_dan_Minuman.nama5)
+print('kandungan makanan dan minuman 5: ',Makanan_dan_Minuman.kandungan5)
+print('kelompok makanan dan minuman 5 : ',Makanan_dan_Minuman.kelompok5)
+print('nama makanan dan minuman 6     : ',Makanan_dan_Minuman.nama6)
+print('kandungan makanan dan minuman 6: ',Makanan_dan_Minuman.kandungan6)
+print('kelompok makanan dan minuman 6 : ',Makanan_dan_Minuman.kelompok6)
+
+print()
+
+#CLASSES N OBJECT (KELAS N OBJEK)
+#cara pada nomor 2
+class Makanan_dan_Minuman:
+    tingkat1   = 'piramida pertama.'
+    nama1      = 'permen, cupcake, cokelat, dan soda.'
+    kelompok1  = 'tidak dikonsumsi setiap hari.'
+    kandungan1 = 'rendah nutrisi, tinggi kalori, lemak, serta gula.'
+    tingkat2   = 'piramida ke-dua.'
+    nama2      = 'mentega dan minyak.'
+    kelompok2  = 'dikonsumsi dengan kuantitas yang sedikit.'
+    kandungan2 = 'lemak jenuh dan asam lemak.'
+    tingkat3   = 'piramida ke-tiga.'
+    nama3      = 'daging, ayam, ikan, telur, dan kacang.'
+    kelompok3  = 'terdapat protein hewani maupun nabati yang membangun atau memperbaiki jaringan tubuh, serta menghasilkan enzim, hormon, dan sumber protein.'
+    kandungan3 = 'protein hewani maupun nabati.'
+    tingkat4   = 'piramida ke-empat.'
+    nama4      = 'susu dan produk susu seperti yogurt dan keju.'
+    kelompok4  = 'dikonsumsi 3x sehari untuk orang dewasa. dikonsumsi 5x sehari untuk anak-anak ataupun remaja yang berusia 9 tahun sampai 18 tahun.'
+    kandungan4 = 'protein, kalsium, dan vitamin B12.'
+    tingkat5   = 'piramida ke-lima.'
+    nama5      = 'bijirin, roti, kentang, pasta, dan nasi.'
+    kelompok5  = 'dikonsumsi 3x sampai 5x sehari.'
+    kandungan5 = 'karbohidrat yang menyediakan energi bagi tubuh dan biji-bijian utuh (whole grains) mengandung serat yang tinggi.'
+    tingkat6   = 'piramida ke-enam.'
+    nama6      = 'salad, sayur dan buah-buahan.'
+    kelompok6  = 'dikonsumsi 3x sampai 7x sehari.'
+    kandungan6 = 'kaya akan vitamin, mineral, serat, dan antioksidan yang penting untuk menjaga kesehatan tubuh secara keseluruhan.'
+#membuat objek dari kelas makanan
+kelompok = Makanan_dan_Minuman()
+print('-- class Makanan dan Minuman --')
+print('memiliki 6 kelompok Makanan dan Minuman: ')
+print('tingkat 1  : ', kelompok.tingkat1)
+print('nama 1     : ', kelompok.nama1)
+print('kelompok 1 : ', kelompok.kelompok1)
+print('kandungan 1: ', kelompok.kandungan1)
+print('tingkat 2  : ', kelompok.tingkat2)
+print('nama 2     : ', kelompok.nama2)
+print('kelompok 2 : ', kelompok.kelompok2)
+print('kandungan 2: ', kelompok.kandungan2)
+print('tingkat 3  : ', kelompok.tingkat3)
+print('nama 3     : ', kelompok.nama3)
+print('kelompok 3 : ', kelompok.kelompok3)
+print('kandungan 3: ', kelompok.kandungan3)
+print('tingkat 4  : ', kelompok.tingkat4)
+print('nama 4     : ', kelompok.nama4)
+print('kelompok 4 : ', kelompok.kelompok4)
+print('kandungan 4: ', kelompok.kandungan4)
+print('tingkat 5  : ', kelompok.tingkat5)
+print('nama 5     : ', kelompok.nama5)
+print('kelompok 5 : ', kelompok.kelompok5)
+print('kandungan 5: ', kelompok.kandungan5)
+print('tingkat 6  : ', kelompok.tingkat6)
+print('nama 6     : ', kelompok.nama6)
+print('kelompok 6 : ', kelompok.kelompok6)
+print('kandungan 6: ', kelompok.kandungan6)
+
+print()
+
+#cara pada nomor 3
+#membuat kelasnya terlebih dahulu
+class Makanan_dan_Minuman:
+    kelompok_makanan_dan_minuman = None
+    nama_makanan_dan_minuman     = None
+    def mengenali (self):
+        print(f"mengenali kelompok makanan dan minuman {self.kelompok_makanan_dan_minuman}. nama makanan dan minumannya ialah {self.nama_makanan_dan_minuman}")
+sayur = Makanan_dan_Minuman()
+sayur.kelompok_makanan_dan_minuman = "kaya akan vitamin, mineral, serat, dan antioksidan yang penting untuk menjaga kesehatan tubuh secara keseluruhan"
+sayur.nama_makanan_dan_minuman = "salad, sayur, dan buah-buahan."
+soda = Makanan_dan_Minuman()
+soda.kelompok_makanan_dan_minuman = "rendah nutrisi, tinggi kalori, lemak, serta gula"
+soda.nama_makanan_dan_minuman = "permen, cupcake, cokelat, dan soda."
+#panggil fungsi mengenali
+sayur.mengenali()
+soda.mengenali()
+
+print()
+
+#cara pada nomor 4
+#metodenya
+class Makanan_dan_Minuman:
+    def __init__(self, tingkat, nama, kelompok, kandung):
+        self.tingkat   = tingkat  #atribut tingkat
+        self.nama      = nama     #atribut nama
+        self.kelompok  = kelompok #atribut kelompok
+        self.kandung   = kandung  #atribut kandungan
+    def konsumsi(self):
+        print(f"makanan dan minuman bertingkat {self.tingkat}.")
+    def tampilan_info(self):
+        print(f"makanan ini bernama {self.nama} mengandung {self.kandung}. kelompok {self.kelompok}.")
+#membuat objek
+makanan_dan_minuman = Makanan_dan_Minuman("piramida ke-dua", "mentega dan minyak", "dikonsumsi dengan kuantitas yang sedikit", "lemak jenuh dan asam lemak")
+#mengakses atribut
+print(makanan_dan_minuman.tingkat)  #output: piramida ke-dua
+print(makanan_dan_minuman.nama)     #output: mentega dan minyak
+print(makanan_dan_minuman.kelompok) #output: dikonsumsi dengan kuantitas yang sedikit
+print(makanan_dan_minuman.kandung)  #output: lemak jenuh dan asam lemak
+#memanggil metode
+makanan_dan_minuman.konsumsi()
+makanan_dan_minuman.tampilan_info()
+
+print()
+
+#INHERITANCE (PENURUNAN)
+#cara pada nomor 5
+#Kelas Induk
+class Makanan_dan_Minuman:
+    def __init__(self, tingkat, nama, kelompok, kandung):
+        self.tingkat  = tingkat
+        self.nama     = nama
+        self.kelompok = kelompok
+        self.kandung  = kandung
+    def tampilan_informasi(self):
+        return f"Makanan ini adalah tingkat {self.tingkat}. contoh nama makanannya adalah {self.nama}. makanan ini mendeskripsikan kelompok {self.kelompok} dan memiliki kandungan {self.kandung}"
+#Kelas Turunan (Daging dan Ikan)
+class Daging_dan_Ikan(Makanan_dan_Minuman):
+    def __init__ (self, tingkat, nama, kelompok, kandung):
+        super().__init__(tingkat, nama, kelompok, kandung)
+        self.kelompok = kelompok
+        self.kandung  = kandung
+    def tampilan_informasi(self):
+        return f"daging dan ikan ini tingkat {self.tingkat}, kelompok {self.kelompok}, serta memiliki kandungan {self.kandung}"
+#Kelas Turunan (Telur dan Kacang-Kacangan)
+class Telur_dan_Kacang_Kacangan(Makanan_dan_Minuman):
+    def __init__ (self, tingkat, nama, kelompok, kandung):
+        super().__init__(tingkat, nama, kelompok, kandung)
+        self.tingkat = tingkat
+        self.nama    = nama
+    def tampilan_informasi(self):
+        return f"nama makanan adalah {self.nama}, makanan ini menduduki tingkat ke {self.tingkat}"
+#Membuat objek dan mencetak informasi
+daging_dan_ikan1 = Daging_dan_Ikan("3", "daging dan ikan", "protein hewani", "protein hewani dan omega-3")
+print(daging_dan_ikan1.tampilan_informasi())
+telur_dan_kacang_kacangan = Telur_dan_Kacang_Kacangan("3", "telur dan kacang-kacangan", "protein hewani maupun nabati", "berprotein dan berserat")
+print(telur_dan_kacang_kacangan.tampilan_informasi())
+
+print()
+
+#ENCAPSULATION N POLYMORPHISM (PENGKAPSULASI / PENGKAPSULAN N POLIMORFISME)
+#PENGKAPSULAN / PENGKAPSULASI(ENCAPSULATION)
+#cara pada nomor 6
+class Makanan_dan_Minuman:
+    def __init__(self, kandung=0):
+        #Atribut private, tidak dapat diakses langsung dari luar kelas
+        self.__kandung = kandung
+#metode untuk menambah nilai kandungan
+    def tambah_kandung(self, amount):
+        if amount > 0:
+            self.__kandung += amount
+            return f"berhasil menambah {amount}. nilai kandungan makanan dan minuman sekarang: {self.__kandung}"
+        else:
+            return "jumlah nilai kandungan tidak valid!"
+#metode untuk mengurangi nilai kandungan
+    def kurangi_kandung(self, amount):
+        if 0 < amount <= self.__kandung:
+            self.__kandung -= amount
+            return f"berhasil mengurangi {amount}. nilai kandungan makanan dan minuman sekarang: {self.__kandung}"
+        else:
+            return "jumlah nilai kandungan tidak valid atau melebihi jumlah yang tersedia!"
+#metode untuk check nilai kandungan
+    def cek_kandung(self):
+        return f"nilai kandungan makanan dan minuman: {self.__kandung}"
+#membuat objek dari kelas Makanan dan Minuman
+makanan_dan_minuman1 = Makanan_dan_Minuman()
+#menambah nilai kandungan
+print(makanan_dan_minuman1.tambah_kandung(101)) #"berhasil menambah 101. nilai kandungan makanan dan minuman sekarang: 101
+#mengecheck nilai kandungan
+print(makanan_dan_minuman1.cek_kandung())       #"nilai kandungan makanan dan minuman: 101"
+#mengurangi nilai kandungan
+print(makanan_dan_minuman1.kurangi_kandung(50)) #"berhasil mengurangi 50. nilai kandungan makanan dan minuman sekarang: 51"
+
+print()
+
+#POLIMORFISME
+#cara pada nomor 7
+class Karbohidrat:
+    def manfaat_dalam_kandungan(self):
+        return "bijirin, roti, kentang, pasta, dan nasi memiliki karbohidrat sebagai sumber energi utama manusia dan protein yang tinggi."
+class Protein:
+    def manfaat_dalam_kandungan(self):
+        return "susu, yogurt, dan keju memiliki kalsium dan sumber protein berkualitas tinggi untuk menjaga kesehatan tulang maupun gigi."
+#fungsi
+def manfaat_dalam_kandungan_Karbohidrat_dan_Protein(makanan_dan_minuman):
+    print(makanan_dan_minuman.manfaat_dalam_kandungan())
+#membuat objek dari kelas Karbohidrat dan Protein
+nasi = Karbohidrat()
+keju = Protein()
+#memanggil fungsi manfaat dalam kandungan Karbohidrat dan Protein
+manfaat_dalam_kandungan_Karbohidrat_dan_Protein(nasi)
+manfaat_dalam_kandungan_Karbohidrat_dan_Protein(keju)
